22bf3387bba41158dde743c0e36dc9d05943a0fc
EC2 Default User <ec2-user@ip-172-31-40-12.us-east-2.compute.internal>
2020-03-23 09:23:44 +0000

    create js file

--- test/test.js
+++ test/test.js
@@ -1,28 +1,44 @@
+
+import * as Slide from '../slide63';
+
+var assert = require('chai').assert;
+
+
+describe("Matching", function() {
+  var array_of_ids = [1,2,3,4,5];
   describe("border_turns_yellow", function() {
 
     function makeTest(x, matched) {
-        array_of_ids = [1,2,3,4,5];
-        valueToRemove = x;
-        others = array_of_ids.filter(array_of_ids => array_of_ids !== valueToRemove);
+        var valueToRemove = x;
+        var others = array_of_ids.filter(array_of_ids => array_of_ids !== valueToRemove);
         
       it(`turns the border color of id ${x} image to yellow if it is the only one`, function() {
           console.log(matched, x);
           if (matched[x] == true){
-            assert.equal(border_turns_yellow(x, others, matched), "rgb(124,252,0)");
+            assert.equal(Slide.border_turns_yellow(x, others, matched), "rgb(124,252,0)");
           }
           else{
-            assert.equal(border_turns_yellow(x, others, matched), "yellow");
+            assert.equal(Slide.border_turns_yellow(x, others, matched), "yellow");
           }
         
       });
     }
 
     for (let x = 1; x <= 5; x++) {
-      matched = {}
+      var matched = {}
       for (let i = 1;i<=5;i++) {
         matched[i] = Math.round(Math.random());
       }
       makeTest(x, matched);
     }
+  });
   
-});
+  describe("Select two images", function() {
+    it("the borders turn green for two matching images", function (){
+      
+    })
+  })
+  
+  
+});  
+
