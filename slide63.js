4d695b94119a92df9ed7d94193b1dcc933767488
chioujiin <a109133409@gmail.com>
2020-04-01 16:55:24 -0500

    added unclick

--- slide63.js
+++ slide63.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
   var clicked = [];
   var matched = {1:false, 2:false, 3:false, 4:false, 5:false};
   $(".box").click(function(){                 
-    border_turns_yellow(this, clicked, matched);
+    Matching(this, clicked, matched);
   });
 
 });
