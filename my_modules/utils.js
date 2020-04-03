93cdb6a621925ed70caa0ad6f6e6810bd05af461
chioujiin <a109133409@gmail.com>
2020-04-03 11:08:53 -0500

    up to date

--- my_modules/utils.js
+++ my_modules/utils.js
@@ -1,46 +1,74 @@
-function Matching(id, clicked, matched){
-  // border_turns_yellow
-  if(clicked.length==0 || clicked[0] == id){
-    border_turns_yellow(id, clicked);
+function Matching(ele, clicked, matched) {
+  if(check_if_matched(ele, matched)){
+    // do nothing.
+    return ;
   }
-}
-
-
-function border_turns_yellow(id, clicked) { 
-  console.log(id, clicked)
-  if(clicked.length==0){
-    $(id).css('border-color', 'yellow');
-    clicked.push(id);
-    // return "yellow"
-  }
-  else if(clicked[0] == id){
-    $(clicked[0]).css('border-color', 'black');
-    clicked.pop();
-    // return "black"
+  else if(clicked.length>0){
+    if (check_if_same_row(ele, clicked)) {
+      // cannot click the imags in the same row other than itself
+      if (check_if_match(ele, clicked)) {
+        border_turns_black(ele, clicked);
+        clicked.pop();
+      }
+    }
+    else if(check_if_match(ele, clicked)){
+      // matching
+      border_turns_green(ele);
+      border_turns_green(clicked[0]);
+      clicked.pop();
+      matched[$(ele).attr("id")] = true;
+    }   
+    else{     
+      // not matching    
+      border_turns_red(ele);
+      border_turns_red(clicked[0]);
+      clicked.pop();
+    }
   }
   else{
-    // border_turns_red()
-    // return "red"
+    // click one image
+    border_turns_yellow(ele, clicked);
+    clicked.push(ele);
   }
 }
 
-function check_if_match(id){      
-//   if (clicked[0].innerHTML == $(id).attr("id")){                
-//     matched[clicked[0].innerHTML] = true;
-//     clicked.shift();
-//     return true
-//   }
-//   else{
-//     return false
-//   }
+// check if the image is at the same row of the clicked image
+function check_if_same_row(ele, clicked) {
+  var clicked_item = clicked[0];
+  var clicked_parent = $(clicked_item).parent();
+  var ele_parent = $(ele).parent();
+
+  return $(clicked_parent).attr("class") == $(ele_parent).attr("class");
+}
+
+// check if the image has same id as the clicked image
+function check_if_match(ele, clicked){ 
+  var clicked_item = clicked[0];
+  return $(clicked_item).attr("id") == $(ele).attr("id");
+}
+
+// check if the image is already matched
+function check_if_matched(ele, matched) {
+  var id = $(ele).attr("id");
+  return matched.hasOwnProperty(id) && matched[id];
+}
+
+function border_turns_yellow(id) {
+  $(id).css('border-color', 'yellow');
 }
 
 // when images match, call this function for border changes
-function border_turns_green(id1, id2){
-  // return "rgb(124,252,0)";
+function border_turns_green(id){
+  $(id).css('border-color', 'rgb(124, 252, 0)');
 }
 
 // when images doesn't match, call this function for border changes
-function border_turns_red(id1, id2){
-  // return "red"
+function border_turns_red(id){
+  $(id).css('border-color', 'red');
 }
+
+function border_turns_black(id){
+  $(id).css('border-color', 'black');
+}
+
+module.exports = {}
--- slide63.js
+++ slide63.js
@@ -1,10 +1,7 @@
-// function called when a div is selected, check if only one and turn yellow
-
 $(document).ready(function () {
   var clicked = [];
-  var matched = {1:false, 2:false, 3:false, 4:false, 5:false};
-  $(".box").click(function(){                 
+  var matched = {};
+  $(".box").click(function(){
     Matching(this, clicked, matched);
   });
-
 });
