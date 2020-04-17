c98235e6cecc188b7acd36dd1e34a0cf5aa82a86
EC2 Default User <ec2-user@ip-172-31-40-12.us-east-2.compute.internal>
2020-04-17 01:07:32 +0000

    test

--- test/green.js
+++ test/green.js
@@ -1,42 +1,52 @@
-var jsdom = require('jsdom');
-const { JSDOM } = jsdom;
-const { assert, expect } = require("chai");
-const { should } = require("mocha");
+// var jsdom = require('jsdom');
+// const { JSDOM } = jsdom;
+// const { assert, expect } = require("chai");
+// const { should } = require("mocha");
+// var jQuery = require('jquery')
 
-var jQuery = require('jquery')
+var expect = chai.expect;
 
 describe("Select two images", function() {
-  var top_rows;
-  var bot_rows;
+  var top_row;
+  var bot_row;
   before(function() {
-    // top_rows = $(".row-1");
-    // bottom_rows = $(".row-2");
-    // return;
-    return JSDOM.fromFile("./index.html").then((dom) => {
-        global.document = dom.window.document;
-        global.windom = document.defaultView;
-        global.$ = jQuery(windom);
-        top_rows = $(".row-1");
-        bot_rows = $(".row-2");
-    })
+    top_row = $(".row-1");
+    bot_row = $(".row-2");
+    // return JSDOM.fromFile("./index.html").then((dom) => {
+    //     global.document = dom.window.document;
+    //     global.window = document.defaultView;
+    //     global.$ = jQuery(window);
+    //     top_rows = $(".row-1");
+    //     bot_rows = $(".row-2");
+    // })
   });
   
   it("the borders turn green for two matching images", function (){
-    // click image by id
-
-    // $('#1').trigger('click');
-    // $('#1').css('border-color').should.equal('yellow');
-    // assert.equal($('#1').css('border-color'), 'yellow');
-    // $('#1').css('border-color', 'yellow');
-    // expect($('#1').attr('style')).eq('yellow');
     var img_id = 1;
-    var selected_image_1 = top_rows.find(`#${img_id}`);
-    var selected_image_2 = bot_rows.find(`#${img_id}`);
+    var selected_image_1 = top_row.find(`#${img_id}`);
+    var selected_image_2 = bot_row.find(`#${img_id}`);
     $(selected_image_1).trigger("click");
-    expect(selected_image_1.css("border-color")).eq("yellow");
+    expect(`#${img_id}`).eq('#1')
+    expect(selected_image_1.css("border-color")).eq("rgb(255, 255, 0)");
     
-    // $(selected_image_2).trigger("click");
-    // expect(selected_image_1.css("border-color")).eq("rgb(124,252,0)");
-    // expect(selected_image_2.css("border-color")).eq("rgb(124,252,0)");
+    $(selected_image_2).trigger("click");
+    expect(selected_image_1.css("border-color")).eq("rgb(124, 252, 0)");
+    expect(selected_image_2.css("border-color")).eq("rgb(124, 252, 0)");
+  })
+  
+  it("the matching image will not turn yellow after clicking it", function (){
+    var img_id = 4;
+    var selected_image_1 = top_row.find(`#${img_id}`);
+    var selected_image_2 = bot_row.find(`#${img_id}`);
+    $(selected_image_1).trigger("click");
+    expect(selected_image_1.css("border-color")).eq("rgb(255, 255, 0)");
+    
+    $(selected_image_2).trigger("click");
+    expect(selected_image_1.css("border-color")).eq("rgb(124, 252, 0)");
+    expect(selected_image_2.css("border-color")).eq("rgb(124, 252, 0)");
+    
+    $(selected_image_1).trigger("click");
+    expect(selected_image_1.css("border-color")).not.eq("rgb(255, 255, 0)");
+    expect(selected_image_1.css("border-color")).eq("rgb(124, 252, 0)");
   })
 })
